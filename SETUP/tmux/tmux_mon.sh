760add829b7031a7c54d922655de5524df737e11
Jaegeuk Kim <jaegeuk@kernel.org>
2016-08-03 17:20:46 -0700

    set proxy
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- SETUP/tmux/tmux_mon.sh
+++ SETUP/tmux/tmux_mon.sh
@@ -8,46 +8,61 @@ Y4="ssh jaegeuk@yosemite-4"
 _dstat()
 {
 	tmux selectp -t $1
-	tmux send-keys "dstat -cmdn -D sdb,sdc -N eth0, eth5"
+	tmux send-keys "clear"
 	tmux send-keys KPenter
+	tmux send-keys "dstat --fs -cmdn -D sdb,sdc -N eth0,eth5"
+	tmux send-keys KPenter
+}
+
+_split_v_4()
+{
+	tmux selectp -t $1
+	tmux splitw -h -p 50 $Y3
+	tmux splitw -v -p 50 $Y4
+	tmux selectp -t $1
+	tmux splitw -v -p 50 $Y2
+
+	tmux selectp -t $1
+	tmux splitw -v -p 95 $Y1
+	tmux selectp -t $(($1+2))
+	tmux splitw -v -p 95 $Y2
+	tmux selectp -t $(($1+4))
+	tmux splitw -v -p 95 $Y3
+	tmux selectp -t $(($1+6))
+	tmux splitw -v -p 95 $Y4
 }
 
 _split_4()
 {
 	tmux selectp -t $1
-	tmux splitw -h -p 50 $Y2
 	tmux splitw -h -p 50 $Y3
+	tmux splitw -h -p 50 $Y4
 	tmux selectp -t $1
-	tmux splitw -h -p 50 $Y1
+	tmux splitw -h -p 50 $Y2
 }
 
 _base_view()
 {
-	tmux list-window | grep "0: base"
-	if [ $? -eq 0 ]; then
-		return
-	fi
-
-	tmux new-window -n base $Y1
 	tmux selectp -t 0
 	tmux splitw -v -p 50 $Y1
 
-	_split_4 0
-	_split_4 4
+	_split_v_4 0
 
 	# set watch
-	for i in `seq 0 3`
+	for i in 1 3 5 7
 	do
 		_dstat $i
 	done
 
-	tmux setw synchronize-panes on
+	_split_4 8
+
+#	tmux setw synchronize-panes on
 	tmux move-window -t 0
 }
 
 tmux has-session -t mon
 if [ $? -ne 0 ]; then
-	tmux new-session -d -s mon -n base
+	tmux new-session -d -s mon -n base $Y1
 fi
 
 _base_view
