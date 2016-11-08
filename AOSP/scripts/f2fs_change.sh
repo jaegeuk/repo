50d50f955dcaeabb548fa9701a540d1fa8e16d8c
Jaegeuk Kim <jaegeuk@kernel.org>
2016-11-08 14:15:49 -0800

    for 7.0 update
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- AOSP/scripts/f2fs_change.sh
+++ AOSP/scripts/f2fs_change.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
 OUT=out/target/product/angler/
-VER=angler-mtc19v
+VER=angler-nrd90u
 
 CUR=$PWD
 
 _download()
 {
-	scp $OUT/boot.img jaegeuk@mac:~/Work/$VER/my_boot.img
-	scp $OUT/recovery.img jaegeuk@mac:~/Work/$VER/my_recovery.img
+	scp $OUT/boot.img jaegeuk@mac:~/Work/$VER/F2FS_boot.img
+	scp $OUT/recovery.img jaegeuk@mac:~/Work/$VER/F2FS_recovery.img
+	scp $OUT/system/bin/fsck.f2fs jaegeuk@mac:~/Work/$VER/
+	scp $OUT/system/lib/libsqlite.so jaegeuk@mac:~/Work/$VER/system_lib_libsqlite.so
+	scp $OUT/system/lib64/libsqlite.so jaegeuk@mac:~/Work/$VER/system_lib64_libsqlite.so
 }
 
 _flash_img()
 {
 	fastboot flash boot $OUT/boot.img
 	fastboot flash recovery $OUT/recovery.img
+	#fastboot flash recovery twrp-3.0.2-2-angler.img
+	fastboot flash system $OUT/system.img
+#	fastboot flash cache $OUT/cache.img
+#	fastboot flash userdata $OUT/userdata.img
 }
 
 _flash()
@@ -26,6 +33,8 @@ _flash()
 	adb push $OUT/system/bin/fsck.f2fs /system/bin/
 	adb push $OUT/system/lib/libsqlite.so /system/lib/
 	adb push $OUT/system/lib64/libsqlite.so /system/lib64/
+
+	echo "mount -t ext4 /../vendor /tmp; sync /tmp/build.prop's fingerprint with /system/build.prop"
 }
 
 _patch()
@@ -45,5 +54,5 @@ _patch()
 
 #_patch
 #_download
-_flash_img
-#_flash
+#_flash_img
+_flash
--- AOSP/scripts/make_android.sh
+++ AOSP/scripts/make_android.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rm -rf out/target/product/angler/*.img
+
+# Use 7GB RAM for Jack Server -1GB from 8GB
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx7000m"
+# Killing...
+out/host/linux-x86/bin/jack-admin kill-server
+# Starting...
+out/host/linux-x86/bin/jack-admin start-server
+
 export TARGET_PREBUILT_KERNEL=kernel/arch/arm64/boot/Image.gz-dtb
 
 source build/envsetup.sh
